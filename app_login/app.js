f8062e0cefb53f136aad035aaeeb446e7a6d7373
shashipalla <150792996+shashipalla@users.noreply.github.com>
2024-01-01 19:43:13 +0530

    Add files via upload

---
+++ app_login/app.js
@@ -0,0 +1,11 @@
+
+
+function add(){
+let email = document.getElementById('Email');
+let password = document.getElementById('Password')
+console.log(email.value);
+console.log(password.value);
+
+email.value="";
+password.value ="";
+}
